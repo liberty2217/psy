d09830573671edce8b324dbbf80e4bb794d16e99
BairUrusov <burusov@stagewood.com>
2023-06-14 02:54:05 +0400

    add initial screen

---
+++ src/screens/RegisterScreen/styles.ts
@@ -0,0 +1,39 @@
+import {createStyles} from '../../styles/createStyles';
+
+export const useStyles = createStyles(theme => ({
+  wrapper: {
+    flex: 1,
+    backgroundColor: theme.COLORS.darkSwitch.background,
+  },
+  container: {
+    flex: 1,
+  },
+  logo: {marginTop: 30, alignSelf: 'center'},
+  darkModeButton: {
+    alignItems: 'flex-end',
+    marginRight: 14,
+  },
+  button: {
+    marginHorizontal: 14,
+  },
+  bottomContainer: {
+    flex: 1,
+    marginTop: 30,
+  },
+  bottomText: {alignSelf: 'center', paddingTop: 15},
+  plainText: {
+    fontFamily: theme.FONTS.Regular,
+    color: theme.COLORS.darkSwitch.text,
+  },
+  signUpText: {
+    fontFamily: theme.FONTS.SemiBold,
+    color: theme.COLORS.primaryGreen,
+  },
+  disclaimerText: {
+    textAlign: 'center',
+    marginHorizontal: 20,
+    fontFamily: theme.FONTS.Regular,
+    color: theme.COLORS.gray,
+    fontSize: 10,
+  },
+}));
